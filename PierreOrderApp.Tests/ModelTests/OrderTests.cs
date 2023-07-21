6be5b6a91f0e495b45f6efd2fffcb38978c2c9d5
JaseGrable <jase.grable@gmail.com>
2023-07-21 12:55:24 -0700

    WIP testing

--- PierreOrderApp.Tests/ModelTests/OrderTests.cs
+++ PierreOrderApp.Tests/ModelTests/OrderTests.cs
@@ -20,5 +20,24 @@ namespace PierreOrderApp.Tests
             Order newOrder = new Order("Test Order");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
+        [TestMethod]
+        public void GetDetails_ReturnsDetails_String()
+        {
+            string details = "20 loaves";
+            Order newOrder = new Order(details);
+            string result = newOrder.Details;
+            Assert.AreEqual(details, result);
+        }
+
+        [TestMethod]
+        public void SetDetails_SetDetails_String()
+        {
+            string details = "20 loaves";
+            Order newOrder = new Order(details);
+            string newDetails = "20 pastries";
+            newOrder.Details = newDetails;
+            string result = newOrder.Details;
+            Assert.AreEqual(newDetails, result);
+        }
     }
 }

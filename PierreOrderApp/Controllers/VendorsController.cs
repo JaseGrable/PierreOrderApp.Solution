8e7f39aed0d96dbb59164fec1d209e554c78ab1d
JaseGrable <jase.grable@gmail.com>
2023-07-24 21:03:58 -0700

    Add vendors controller

---
+++ PierreOrderApp/Controllers/VendorsController.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PierreOrderApp.Models;
+
+namespace PierreOrderApp.Controllers
+{
+    public class VendorsController : Controller
+    {
+        [HttpGet("/vendors")]
+        public ActionResult Index()
+        {
+            List<Vendor> allVendors = Vendor.GetAll();
+            return View(allVendors);
+        }
+        [HttpGet("/vendors/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+        [HttpPost("/vendors")]
+        public ActionResult Create(string vendorName)
+        {
+            Vendor newVendor = new Vendor(vendorName);
+            return RedirectToAction("Index");
+        }
+        [HttpGet("/vendors/{id}")]
+        public ActionResult Show(int id)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor selectedVendor = Vendor.Find(id);
+            List<Order> vendorOrders = selectedVendor.Orders;
+            model.Add("vendor", selectedVendor);
+            model.Add("orders", vendorOrders);
+            return View(model);
+        }
+        [HttpPost("/vendors/{vendorID}/orders")]
+        public ActionResult Create(int vendorID, string orderDetail)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor foundVendor = Vendor.Find(vendorID);
+            Order newOrder = new Order(orderDetail);
+            foundVendor.AddOrder(newOrder);
+            List<Order> vendorOrders = foundVendor.Orders;
+            model.Add("orders", vendorOrders);
+            model.Add("vendors", foundVendor);
+            return View("show", model);
+        }
+    }
+}
